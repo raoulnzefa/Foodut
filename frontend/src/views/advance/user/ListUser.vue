dc00d93e348ed748d33fac3c0bb57acfc8c8795d
Elangel <elangelshaday@gmail.com>
2022-03-07 05:42:17 +0700

    Update frontend navbar and delete unused feature

---
+++ frontend/src/views/advance/user/ListUser.vue
@@ -0,0 +1,12 @@
+<!-- =========================================================================================
+  File Name: ECommerceWishList.vue
+  Description: eCommerce Wish List Page
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+  Author: Pixinvent
+  Author URL: http://www.themeforest.net/user/pixinvent
+========================================================================================== -->
+
+<template>
+  <h1>List User</h1>
+</template>
