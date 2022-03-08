21aac30350590e262bd07ee877dd465c5e9dc876
TimothyRay18 <timothy.ray1802@gmail.com>
2022-03-08 19:34:45 +0700

    konflik

--- backend/models/detailProduct.go
+++ backend/models/detailProduct.go
@@ -3,9 +3,6 @@ package models
 type DetailProduct struct {
 	Product  Product `form:"product" json:"product" gorm:"primaryKey"`
 	Quantity int     `form:"quantity" json:"quantity"`
-	// UserID    int `form:"id" json:"id" gorm:"primaryKey"`
-	// ProductID int `form:"productId" json:"productId"`
-	// Quantity  int `form:"quantity" json:"quantity"`
 }
 
 type DetailProductRespone struct {
