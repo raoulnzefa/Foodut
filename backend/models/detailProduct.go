3afff858ef96d119f114b53658c1ebd4cde148b3
Jeddi212 <jedediahf51@gmail.com>
2022-03-08 21:02:08 +0700

    DetailProduct Status 👥

--- backend/models/detailProduct.go
+++ backend/models/detailProduct.go
@@ -4,6 +4,8 @@ type DetailProduct struct {
 	UserID    int `form:"id" json:"id" gorm:"primaryKey;autoIncrement:false"`
 	ProductID int `form:"productId" json:"productId" gorm:"primaryKey;autoIncrement:false"`
 	Quantity  int `form:"quantity" json:"quantity"`
+	Status    int `form:"status" json:"status"`
+	// { 0: wishlist, 1: payed }
 }
 
 type DetailProductRespone struct {
