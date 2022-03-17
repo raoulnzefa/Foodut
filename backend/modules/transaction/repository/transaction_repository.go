8c244b96f626049267932d9bf08d01934450f23d
Jeddi212 <jedediahf51@gmail.com>
2022-03-17 18:35:41 +0700

    Get All Trasaction semi-done 😌

--- backend/modules/transaction/repository/transaction_repository.go
+++ backend/modules/transaction/repository/transaction_repository.go
@@ -1,1 +1,30 @@
 package repository
+
+import (
+	dbController "github.com/Foodut/backend/database"
+	model "github.com/Foodut/backend/modules/transaction/domain/model"
+)
+
+func FindAllTransaction(transactionId []string) []model.Transaction {
+	// Check connection
+	con := dbController.GetConnection()
+
+	var transactions []model.Transaction
+
+	if transactionId != nil {
+		con.Find(&transactions, transactionId[0])
+	} else {
+		con.Find(&transactions)
+	}
+
+	for i := 0; i < len(transactions); i++ {
+		con.Model(&transactions[i]).Association("ProductDetail").Find(&transactions[i].ProductDetail)
+
+		con.Model(&transactions[i].ProductDetail).Association("Picture")
+		for j := 0; j < len(transactions[i].ProductDetail); j++ {
+			con.Model(&transactions[i].ProductDetail[j]).Association("Picture").Find(&transactions[i].ProductDetail[j].Picture)
+		}
+	}
+
+	return transactions
+}
--- backend/modules/transaction/rest-api/controller/transaction_controller.go
+++ backend/modules/transaction/rest-api/controller/transaction_controller.go
@@ -4,23 +4,16 @@ import (
 	"encoding/json"
 	"net/http"
 
-	dbController "github.com/Foodut/backend/database"
-	"github.com/Foodut/backend/modules/transaction/domain/model"
+	model "github.com/Foodut/backend/modules/transaction/domain/model"
+	srvc "github.com/Foodut/backend/modules/transaction/domain/service"
 	rspn "github.com/Foodut/backend/responses"
 )
 
 func GetAllTransactions(w http.ResponseWriter, r *http.Request) {
-	db := dbController.GetConnection()
-	//defer db.Close()
 
-	var transactions []model.Transaction
+	transactionId := r.URL.Query()["id"]
 
-	transactionID := r.URL.Query()["id"]
-	if transactionID != nil {
-		db.Where("transaction_id = ?", transactionID[0]).First(&transactions)
-	} else {
-		db.Find(&transactions)
-	}
+	var transactions []model.Transaction = srvc.SearchById(transactionId)
 
 	// Set response
 	var response rspn.Response
