52ffaed2dcb96b346ea4ebcd109f0085dcde7575
Jeddi212 <jedediahf51@gmail.com>
2022-03-19 16:49:55 +0700

    -> Testing 1️⃣

--- backend/testing/testing1/testing1_test.go
+++ backend/testing/testing1/testing1_test.go
@@ -4,16 +4,25 @@ import (
 	"testing"
 
 	dbController "github.com/Foodut/backend/database"
+	model "github.com/Foodut/backend/modules/transaction/domain/model"
+	repo "github.com/Foodut/backend/modules/transaction/repository"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDatabaseConnection(t *testing.T) {
 	con := dbController.GetConnection()
 
-	if con.Error != nil {
-		t.Fatalf(
-			"Error (%+v) ||| \n"+
-				"Expected to Connect",
-			con.Error,
-		)
-	}
+	require.NoError(t, con.Error)
+}
+
+func TestGetTransaction(t *testing.T) {
+	con := dbController.GetConnection()
+
+	var expected []model.Transaction
+	con.Find(&expected)
+
+	actual := repo.FindAllTransaction(nil)
+
+	assert.Equal(t, len(expected), len(actual))
 }
