02cc59c1716985a6204e971c22ce9f9d07960c1a
fedlyseptian <fedlyseptian.fs@gmail.com>
2022-03-29 00:05:06 +0700

    create EditCustomer

--- backend/modules/user/rest-api/controller/customer_controller.go
+++ backend/modules/user/rest-api/controller/customer_controller.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
+	dbController "github.com/Foodut/backend/database"
+	model "github.com/Foodut/backend/modules/user/domain/model"
 	srvc "github.com/Foodut/backend/modules/user/domain/service"
 	dto "github.com/Foodut/backend/modules/user/rest-api/dto"
 	rspn "github.com/Foodut/backend/responses"
@@ -32,4 +34,37 @@ func PostCustomer(writer http.ResponseWriter, req *http.Request) {
 
 	writer.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(writer).Encode(response)
+}
+
+func EditCustomer(w http.ResponseWriter, r *http.Request) {
+	// Check connection
+	con := dbController.GetConnection()
+
+	// Get email from logged member
+	email := GetEmailFromToken(r)
+
+	var customer model.Customer
+	con.Where("email=?", email).First(&customer)
+
+	if name := r.Form.Get("name"); name != "" {
+		customer.User.Name = name
+	}
+	if address := r.Form.Get("address"); address != "" {
+		customer.Address = address
+	}
+	if password := r.Form.Get("password"); password != "" {
+		customer.User.Password = password
+	}
+
+	result := con.Save(&customer)
+
+	var response rspn.Response
+	if result.Error == nil {
+		response.Response_200("data has been updated")
+	} else {
+		response.Response_400(result)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
